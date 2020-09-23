e4ab4dac94042318ba0bb429d7b5e2f8b01f88c3
CrispyNipsy <magnus.borregaard@gmail.com>
2020-09-23 23:08:42 +0200

    fireship internal nav done

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,83 +1,214 @@
 <template>
-	<nav class="navbar">
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a href="" class="nav-link">
-					<fa class="link-icon" icon="cat" aria-hidden="true" focusable="false" />
-					<span class="link-text">Cats</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="" class="nav-link">
-					<fa class="link-icon" icon="dog" aria-hidden="true" focusable="false" />
-					<span class="link-text">Dogs</span>
-				</a>
-			</li>
-		</ul>
-	</nav>
+  <nav class="navbar">
+    <ul class="navbar-nav">
+      <li class="logo">
+        <NuxtLink to="/" class="nav-link">
+          <img class="logo__img" :src="logo.img" :alt="logo.alt" />
+          <fa
+            class="navbar__icon"
+            icon="angle-double-right"
+            aria-hidden="true"
+            focusable="false"
+          />
+        </NuxtLink>
+      </li>
+
+      <li v-for="link of links" :key="link.to" class="nav-item">
+        <NuxtLink :to="link.to" class="nav-link">
+          <fa
+            class="nav-link__icon"
+            :icon="link.icon"
+            aria-hidden="true"
+            focusable="false"
+          />
+          <span class="nav-link__label">{{ link.label }}</span>
+        </NuxtLink>
+      </li>
+
+
+      <li class="nav-item">
+        <a role="button" class="nav-link" @click="toggleDarkMode">
+          <fa
+            class="nav-link__icon"
+            :icon="darkModeIcon"
+            aria-hidden="true"
+            focusable="false"
+          />
+          <span class="nav-link__label">{{ darkModeLabel }}</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-	export default {
-		name: 'Navbar'
+export default {
+  name: 'Navbar',
+  props: {
+    logo: {
+      // { img, alt }
+      type: Object,
+      required: true,
+    },
+
+    links: {
+			// { icon, label, to }
+      type: Array,
+      required: true,
+    },
+	},
+	computed: {
+		darkModeIcon() {
+			return this.$store.state.darkMode ? 'moon' : 'sun'
+		},
+		darkModeLabel() {
+			return this.$store.state.darkMode ? 'Light Mode' : 'Dark Mode'
+		}
+	},
+	methods: {
+		toggleDarkMode() {
+			this.$store.commit('toggleDarkMode')
+		}
 	}
+}
 </script>
 
 <style lang="scss" scoped>
 .navbar {
-	// margin-right: 5rem;
-	padding: 1rem;
-	width: 5rem;
-	height: 100vh;
-	position: fixed;
-	background: var(--sec-clr);
+  --transition-speed: 200ms;
+  
+  position: fixed;
+  background: var(--clr-sec);
+	transition: width var(--transition-speed) ease;
+	overflow: hidden;
 
-	&:hover {
-		width: 16rem;
+  &-nav {
+    list-style: none;
+    height: 100%;
 
-		.link-text {
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__icon {
+    min-width: 2rem;
+    min-height: 2rem;
+    margin: 0 1.5rem;
+    transition: var(--transition-speed) ease;
+    color: var(--clr-sec);
+    transform: rotate(0deg);
+  }
+
+  &:hover {
+    .logo__img {
 			display: block;
-		}
+    }
+
+    .navbar__icon {
+      transform: rotate(180deg);
+      margin-left: auto;
+    }
+  }
+}
+
+.nav-item {
+	width: 100%;
+	
+	&:last-child {
+		margin-top: auto;
+	}
+}
+
+.nav-link {
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  text-decoration: none;
+  color: var(--clr-sec-light);
+  transition: var(--transition-speed) ease;
+  font-weight: 600;
+
+  &:hover {
+    background: var(--clr-sec-dark);
+
+    & > .nav-link__icon {
+      color: var(--clr-sec-light);
+    }
+  }
+
+  &__icon {
+    min-width: 2rem;
+    min-height: 2rem;
+    margin: 0 1.5rem;
+    color: var(--clr-sec-dark);
+    transition: color var(--transition-speed) ease;
+	}
+}
+
+.nav-link__label,
+.logo__img {
+	display: none;
+  margin-left: 1rem;
+	font-size: 1.2rem;
+	white-space: nowrap;
+}
+
+.logo {
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+  text-align: center;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  letter-spacing: 0.3ch;
+  width: 100%;
+  background: var(--clr-sec-dark);
+  color: var(--clr-txt);
+
+  &__img {
+    max-height: 3rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+	.navbar {
+		bottom: 0;
+		width: 100vw;
+		height: 5rem;
 	}
 
 	.navbar-nav {
-		list-style: none;
-		height: 100%;
-
-		padding: 0;
-		margin: 0;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+		flex-direction: row;
 	}
 
-	.nav-item {
-		width: 100%;
-
-		&:last-child {
-			margin-top: auto;
-		}
+	.logo {
+		display: none;
 	}
 
 	.nav-link {
-		display: flex;
-		align-items: center;
-		height: 5rem;
-		color: var(--txt-clr);
-		text-decoration: none;
-
-		.link-icon {
-			min-width: 2rem;
-			margin: 0 1.5rem;
-		}
+		justify-content: center;
 	}
-
-	.link-text {
-		display: none;
-		margin-left: 1rem;
-	}
-
 
 }
+@media only screen and (min-width: 600px) {
+	.navbar {
+		top: 0;
+		width: 5rem;
+		height: 100vh;
+		
+		&:hover {
+			width: 16rem;
 
+			.nav-link__label {
+				display: inline;
+				transition: opacity var(-transition-speed);
+			}
+		}
+	}
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-		<!-- Set a margin-left on main content equal to navbar's width (default 5 rem) -->
-    <Navbar/>
+		<!-- Set a margin-left on main content equal to navbar's width (default 5 rem) on desktop only -->
+    <Navbar :logo="logo" :links="navLinks" />
 
 		<main>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque est sunt vel tempora illum ipsa magni commodi quibusdam, perferendis laboriosam cupiditate suscipit! Adipisci harum nam facere sint aspernatur nihil dignissimos eligendi, quo ad ullam iusto mollitia blanditiis non saepe eum ut incidunt tempora ex corporis soluta vel necessitatibus. Explicabo labore quia ipsa reprehenderit cum eveniet obcaecati, eos iure, sequi nisi sapiente deserunt repellendus dolorum ipsam, excepturi quam accusamus dicta perspiciatis! Odit tempore perferendis dicta nostrum ipsum laudantium quidem ducimus ex dolor hic illo neque nemo illum saepe nulla consectetur nisi dignissimos porro at, vero maxime eligendi vitae, sapiente in. Obcaecati iste, mollitia culpa eum nisi sit, assumenda optio nihil nesciunt aliquam suscipit autem laudantium omnis ipsa reiciendis error in similique porro asperiores laboriosam soluta aliquid. Vitae officiis suscipit nulla iure cumque aliquam facilis laboriosam tempora aperiam, non ipsum aliquid quod, consectetur, dignissimos ipsam. Aspernatur, praesentium magni facilis harum tempore mollitia sunt voluptatibus eum voluptas dolor nulla accusamus sint fugiat illo ipsum laboriosam numquam quidem sit alias vitae asperiores incidunt. Vel sunt eligendi nesciunt. Rem, voluptatibus nemo doloribus illo quam repellendus voluptates. Totam ratione tenetur nam, laboriosam, reprehenderit quo placeat optio recusandae enim nobis, veritatis debitis nisi. Commodi quasi nam praesentium?
@@ -10,12 +10,33 @@
 </template>
 
 <script>
+const img = require('@/assets/placeholders/insta.png')
+
 export default {
+	data() {
+		return {
+			logo: {
+				img,
+				alt: 'instagram-logo'
+			},
+
+			navLinks: [
+				{ to: '/', icon: 'home', label: 'Home'},
+				{ to: '/404', icon: 'skull', label: '404'},
+			]
+		}
+	}
 }
 </script>
 
 <style lang="scss">
 main {
 	margin-left: 5rem;
+}
+
+@media only screen and (max-width: 600px) {
+	main {
+		margin: 0;
+	}
 }
 </style>
